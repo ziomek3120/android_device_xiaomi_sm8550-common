127d0d093051fa3d5a69eeedf94472a40d3aa116
ziomek3120 <unboxorg@gmail.com>
2026-01-02 03:15:11 +0100

    sm8550-common: Change vendor/lineage to vendor/infinity, remove lineage.dependencies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -166,7 +166,7 @@ $(foreach p, $(call to-upper, $(BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST))
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := false
-include vendor/lineage/config/BoardConfigReservedSize.mk
+include vendor/infinity/config/BoardConfigReservedSize.mk
 
 # Platform
 TARGET_BOARD_PLATFORM := kalama
